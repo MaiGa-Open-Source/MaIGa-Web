505a484e2d08a22f4430f93ee7a6b11f0d43880b
Wang Jiajie, CIS-2 <jiajie.wang@lingyue-digital.com>
2021-03-30 14:42:17 +0800

    feat:remove icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -13,7 +12,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
